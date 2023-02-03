f123ef0d6dd5681f7710169fdeacb3c6f4a74e4d
AlexRepaire <repaire.alexandre@gmail.com>
2023-02-03 12:16:34 +0100

    First commit

---
+++ src/setupDatabase.ts
@@ -0,0 +1,22 @@
+import Logger from "bunyan";
+import mongoose from "mongoose";
+import { config } from "./config";
+
+const log: Logger = config.createLogger("setupDatabase");
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info("connexion success to database");
+      })
+      .catch((error) => {
+        log.error(`Error connected to database`, error);
+        return process.exit(1);
+      });
+  };
+  connect();
+
+  mongoose.connection.on("disconnected", connect);
+};
