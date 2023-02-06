f53893863fd6d4f562cc83ad15f2c8cb70bbaac0
AlexRepaire <repaire.alexandre@gmail.com>
2023-02-06 16:03:43 +0100

    first commit

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,22 +1,22 @@
-import Logger from "bunyan";
-import mongoose from "mongoose";
-import { config } from "./config";
+import Logger from 'bunyan';
+import mongoose from 'mongoose';
+import { config } from './config';
 
-const log: Logger = config.createLogger("setupDatabase");
+const log: Logger = config.createLogger('setupDatabase');
 
 export default () => {
   const connect = () => {
     mongoose
       .connect(`${config.DATABASE_URL}`)
       .then(() => {
-        log.info("connexion success to database");
+        log.info('connexion success to database');
       })
       .catch((error) => {
-        log.error(`Error connected to database`, error);
+        log.error('Error connected to database', error);
         return process.exit(1);
       });
   };
   connect();
 
-  mongoose.connection.on("disconnected", connect);
+  mongoose.connection.on('disconnected', connect);
 };
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,32 +1,22 @@
-import {
-  Application,
-  json,
-  NextFunction,
-  Request,
-  Response,
-  urlencoded,
-} from "express";
-import http from "http";
-import cors from "cors";
-import helmet from "helmet";
-import hpp from "hpp";
-import compression from "compression";
-import cookieSession from "cookie-session";
-import HTTP_STATUS from "http-status-codes";
-import { Server } from "socket.io";
-import { createClient } from "redis";
-import { createAdapter } from "@socket.io/redis-adapter";
-import "express-async-errors";
-import { config } from "./config";
-import applicationRoutes from "./routes";
-import {
-  CustomError,
-  IErrorResponse,
-} from "./shared/globals/helpers/error-handler";
-import Logger from "bunyan";
+import { Application, json, NextFunction, Request, Response, urlencoded } from 'express';
+import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import { Server } from 'socket.io';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
+import 'express-async-errors';
+import { config } from './config';
+import applicationRoutes from './routes';
+import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
+import Logger from 'bunyan';
 
 const SERVER_PORT = 5050;
-const log: Logger = config.createLogger("server");
+const log: Logger = config.createLogger('server');
 
 export class ChattyServer {
   private app: Application;
@@ -46,28 +36,28 @@ export class ChattyServer {
   private securityMiddleware(app: Application): void {
     app.use(
       cookieSession({
-        name: "session",
+        name: 'session',
         keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
         maxAge: 24 * 7 * 3600000,
-        secure: config.NODE_ENV !== "local",
+        secure: config.NODE_ENV !== 'local'
       })
     );
     app.use(hpp());
     app.use(helmet());
     app.use(
       cors({
-        origin: "*",
+        origin: '*',
         credentials: true,
         optionsSuccessStatus: 200,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
       })
     );
   }
 
   private standardiddleware(app: Application): void {
     app.use(compression());
-    app.use(json({ limit: "50mb" }));
-    app.use(urlencoded({ extended: true, limit: "50mb" }));
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
   }
 
   private routeMiddleware(app: Application): void {
@@ -75,26 +65,17 @@ export class ChattyServer {
   }
 
   private globalErrorHandler(app: Application): void {
-    app.all("*", (req: Request, res: Response) => {
-      res
-        .status(HTTP_STATUS.NOT_FOUND)
-        .json({ message: `${req.originalUrl} not found` });
+    app.all('*', (req: Request, res: Response) => {
+      res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found` });
     });
 
-    app.use(
-      (
-        error: IErrorResponse,
-        _req: Request,
-        res: Response,
-        next: NextFunction
-      ) => {
-        log.error(error);
-        if (error instanceof CustomError) {
-          return res.status(error.statusCode).json(error.serializeErrors());
-        }
-        next();
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+      log.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.statusCode).json(error.serializeErrors());
       }
-    );
+      next();
+    });
   }
 
   private async startServer(app: Application): Promise<void> {
@@ -112,8 +93,8 @@ export class ChattyServer {
     const io: Server = new Server(httpServer, {
       cors: {
         origin: config.CLIENT_URL,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      }
     });
     const pubClient = createClient({ url: config.REDIS_HOST });
     const subClient = pubClient.duplicate();
@@ -130,5 +111,6 @@ export class ChattyServer {
     });
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   private socketIOConnections(io: Server): void {}
 }
